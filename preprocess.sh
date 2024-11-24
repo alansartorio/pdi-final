9d7e6e1415e9ee9857f35d5881a9aaa90ea8f2c4
Alan Sartorio <alan42ga@hotmail.com>
2024-11-24 20:13:29 -0300

    add sample images

---
+++ preprocess.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rm -rf preprocessed
+mkdir -p preprocessed
+
+find original -type f -name "*.jpg" | \
+    while read file;
+    do
+        basename=${file#original/}
+        output=preprocessed/$(echo $basename | sed 's:/:_:g');
+        echo $output
+        magick $file -resize 10% -gravity center -crop 300x300+0+0 +repage $output &&\
+            exiftool -overwrite_original -all= $output;
+    done
