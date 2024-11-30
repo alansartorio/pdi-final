67fbab87911a986dce3f905851380f3d58235f01
Alan Sartorio <alan42ga@hotmail.com>
2024-11-29 22:13:13 -0300

    add complete explanation for finding face

--- find_face.py
+++ find_face.py
@@ -2,42 +2,9 @@ from typing import Optional
 import cv2
 import numpy as np
 from cv2.typing import MatLike
-import math
 
 from utils import Point, Quad
-
-
-def all_right_angles(shape: MatLike):
-    assert len(shape) >= 3
-    cycled_approx = np.append(shape.copy(), [shape[0], shape[1]], axis=0)
-    for a, b, c in zip(cycled_approx, cycled_approx[1:], cycled_approx[2:]):
-        ab = b[0] - a[0]
-        bc = c[0] - b[0]
-        inner = np.dot(ab, bc) / (np.linalg.vector_norm(ab) * np.linalg.vector_norm(bc))
-        if inner < -1 or inner > 1:
-            return False
-        angle = math.acos(inner)
-        right_angle = abs(angle - math.pi / 2) < math.pi / 10
-        almost_360 = abs(angle - math.pi * 2) < math.pi / 5
-        almost_180 = abs(angle - math.pi) < math.pi / 5
-        if not right_angle and not almost_360 and not almost_180:
-            return False
-    return True
-
-
-def ones_circle(size: int):
-    radius = size // 2
-    arr = np.arange(-radius, radius + 1) ** 2
-    out = np.add.outer(arr, arr) < radius**2
-    return out.astype(np.uint8)
-
-
-def does_contour_touch_border(contour: MatLike, width, height):
-    for ((x, y),) in contour:
-        if x == 0 or y == 0 or x == width - 1 or y == height - 1:
-            return True
-
-    return False
+import utils
 
 
 def find_face(img: MatLike, debug=False, extra_debug=False) -> Optional[Quad]:
@@ -49,68 +16,87 @@ def find_face(img: MatLike, debug=False, extra_debug=False) -> Optional[Quad]:
     if extra_debug:
         cv2.imshow("original", img)
 
+    # Detectar bordes
     edges = cv2.Canny(img, 100, 170)
+    # Invertir para conseguir superficies
     ret, edges = cv2.threshold(255 - edges, 240, 255, cv2.THRESH_BINARY)
 
     if extra_debug:
         cv2.imshow("edges1", edges)
-    edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, ones_circle(4))
+    # Eliminar piezas chicas
+    edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, utils.ones_circle(4))
 
     if extra_debug:
         cv2.imshow("edges2", edges)
-    edges = cv2.erode(edges, ones_circle(4))
+    # Aumentar separacion entre superficies
+    edges = cv2.erode(edges, utils.ones_circle(4))
 
     if debug:
         cv2.imshow("edges", edges)
 
     contour_image = np.zeros_like(img)
 
+    # Encontrar contornos de superficies
     contours, hierarchy = cv2.findContours(
         edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
-    hierarchy = tuple(*hierarchy)
 
     contour_image_approx = np.zeros_like(img)
 
     cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 1)
-    areas = [cv2.contourArea(contour) for contour in contours]
 
     filtered_contours = np.zeros_like(edges)
 
+    # Filtrar contornos de cuadrados o grupos de cuadrados
+    hierarchy = tuple(*hierarchy)
+    areas = [cv2.contourArea(contour) for contour in contours]
     for (i, contour), (next, previous, first_child, parent) in zip(
         enumerate(contours), hierarchy
     ):
+        # Calcular suma de areas de contornos hijos
         children_area = 0
         next = first_child
         while next != -1:
             children_area += areas[next]
             next = hierarchy[next][0]
+
+        # Si tiene hijos y el area supera cierto valor se descarta, se asume que los cuadrados tienen color solido
         if children_area > 0 and children_area > minimum_face_size**2:
             cv2.drawContours(contour_image_approx, [contour], -1, (255, 0, 255), 1)
             cv2.drawContours(
                 contour_image_approx, contours, first_child, (0, 255, 255), 3
             )
             continue
-        if does_contour_touch_border(contour, width, height):
+
+        # Si el contorno toca un borde de la imagen se descarta
+        if utils.does_contour_touch_border(contour, width, height):
             continue
-        # epsilon = minimum_face_size / 3 * 4 * .1
+
+        # Aproximar una figura simplificada al contorno
         epsilon = 0.05 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
+
+        # Si tiene menos de 4 vertices, se descarta
         if len(approx) < 4:
             cv2.drawContours(contour_image_approx, [approx], -1, (0, 0, 255), 1)
             continue
 
-        if not all_right_angles(approx):
+        # Si algun angulo no es recto (o similar) se descarta
+        if not utils.all_right_angles(approx):
             cv2.drawContours(contour_image_approx, [approx], -1, (0, 0, 255), 1)
             continue
+
         contour_area = areas[i]
+        # Si el area del contorno no está en el rango valido, se descarta
         if (
             contour_area < (minimum_face_size / 3) ** 2
             or contour_area > maximum_face_size**2
         ):
             cv2.drawContours(contour_image_approx, [approx], -1, (255, 0, 0), 1)
             continue
+
         cv2.drawContours(contour_image_approx, [approx], -1, (0, 255, 0), 1)
+        # Se dibuja el contorno en una nueva imagen
         cv2.drawContours(filtered_contours, [approx], -1, (255,), -1)
 
     if extra_debug:
@@ -119,12 +105,14 @@ def find_face(img: MatLike, debug=False, extra_debug=False) -> Optional[Quad]:
     if debug:
         cv2.imshow("contours approx", contour_image_approx)
 
+    # Cerrar agujeros entre stickers/cuadrados
     filtered_contours = cv2.morphologyEx(
-        filtered_contours, cv2.MORPH_CLOSE, np.ones((31, 31), dtype=np.uint8)
+        filtered_contours, cv2.MORPH_CLOSE, utils.ones_circle(31)
     )
     if extra_debug:
         cv2.imshow("blurred filtered contours", filtered_contours)
 
+    # Buscar contornos para encontrar cara del cubo
     contours, hierarchy = cv2.findContours(
         filtered_contours, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
@@ -132,15 +120,19 @@ def find_face(img: MatLike, debug=False, extra_debug=False) -> Optional[Quad]:
     areas = []
 
     for contour in contours:
-        contour_area = cv2.contourArea(contour)
+        # Aproximar contorno a figura simple
         epsilon = 0.08 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
+        # Descartar contornos no rectangulares
         if len(approx) != 4:
             continue
+        contour_area = cv2.contourArea(contour)
         areas.append((contour_area, approx))
 
     if len(areas) == 0:
         return None
+
+    # Elegir el contorno con mayor area
     face_contour = max(areas, key=lambda i: i[0])[1]
 
     output_roi = img.copy()
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 from typing import Iterable, NamedTuple
 
+from cv2.typing import MatLike
 from numpy.typing import NDArray
 import numpy as np
 import cv2
+import math
 
 
 Image = NDArray[np.uint8]
@@ -87,3 +89,36 @@ def crop_square(img, size, interpolation=cv2.INTER_AREA):
     resized = cv2.resize(crop_img, (size, size), interpolation=interpolation)
 
     return resized
+
+
+def all_right_angles(shape: MatLike):
+    assert len(shape) >= 3
+    cycled_approx = np.append(shape.copy(), [shape[0], shape[1]], axis=0)
+    for a, b, c in zip(cycled_approx, cycled_approx[1:], cycled_approx[2:]):
+        ab = b[0] - a[0]
+        bc = c[0] - b[0]
+        inner = np.dot(ab, bc) / (np.linalg.vector_norm(ab) * np.linalg.vector_norm(bc))
+        if inner < -1 or inner > 1:
+            return False
+        angle = math.acos(inner)
+        right_angle = abs(angle - math.pi / 2) < math.pi / 10
+        almost_360 = abs(angle - math.pi * 2) < math.pi / 5
+        almost_180 = abs(angle - math.pi) < math.pi / 5
+        if not right_angle and not almost_360 and not almost_180:
+            return False
+    return True
+
+
+def ones_circle(size: int):
+    radius = size // 2
+    arr = np.arange(-radius, radius + 1) ** 2
+    out = np.add.outer(arr, arr) < radius**2
+    return out.astype(np.uint8)
+
+
+def does_contour_touch_border(contour: MatLike, width, height):
+    for ((x, y),) in contour:
+        if x == 0 or y == 0 or x == width - 1 or y == height - 1:
+            return True
+
+    return False
